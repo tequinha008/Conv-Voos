adcf3421033f1a148b2c565695a8d45567129f20
tequinha008 <202507386981@alunos.estacio.br>
2026-08-19 12:42:39 -0300

    Add configuration for Supabase and dynamic responder

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://serzfixqqznmclzlevqp.supabase.co",
+  supabasePublishableKey: "sb_publishable_tBsoHjBrZFmzWBhaQ6rxbQ_qqOzHNX9",
+  funcaoAdicionarCodigo: "dynamic-responder"
+};
